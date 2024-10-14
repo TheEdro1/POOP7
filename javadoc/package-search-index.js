537527442a7ae4c95b4dc82f2435343a48df7726
TheEdro1 <162408477+TheEdro1@users.noreply.github.com>
2024-10-14 14:51:14 -0600

    Adicion de javadoc

---
+++ javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"poop7"},{"l":"zoologico"}];updateSearchResults();
---
+++ javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"zoologico","l":"Animal"},{"p":"poop7","l":"Empleado"},{"p":"poop7","l":"Gerente"},{"p":"poop7","l":"POOP7"}];updateSearchResults();
